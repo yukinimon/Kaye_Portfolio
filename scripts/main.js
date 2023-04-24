81558edef892a8b38e63324c9556b69dda4e1bd0
Jay-Em <joshmatthewtalplacido@gmail.com>
2023-04-24 15:27:23 +0800

    Make the Hambogo menu

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  document.getElementById("open_menu").classList.toggle("hide");
+  document.getElementById("close_menu").classList.toggle("hide");
+  document.getElementById("links").classList.toggle("hide");
+}
