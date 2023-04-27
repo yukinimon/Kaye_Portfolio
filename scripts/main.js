1251504b526298428c4ae8d7593d561e8ce3d0ae
yukinimon <krischellecamama@gmail.com>
2023-04-27 21:39:24 +0800

    Added the disable-scroll for the navBar

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 function toggleMenu() {
+  document.querySelector('body').classList.toggle("disable-scroll");
   document.getElementById("open_menu").classList.toggle("hide");
   document.getElementById("close_menu").classList.toggle("hide");
   document.getElementById("nav").classList.toggle("hide");
